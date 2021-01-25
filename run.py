cfea684e950ca52a2b64eeb95485278c0ab1e95d
duy <duypham12241999@gmail.com>
2021-01-24 22:13:24 -0800

    make run.py init robots sequentially

--- run.py
+++ run.py
@@ -7,14 +7,13 @@ dir_path = os.path.dirname(os.path.realpath(__file__))
                                                                 
 vfirm_dir = dir_path + "/Virtual-Firmware-grSim"
 vfirm_cmd = "./vfirm.exe"
-vfirm_port_base = 8800
+vfirm_port_base = 8000
 vfirm_is_blue = "1"
 
 tritonBot_dir = dir_path + "/TritonBot/build/"
 tritonBot_cmd = "./TritonBot.exe"
 tritonBot_virtual = "-v"
 tritonBot_port_base_base = 6000
-
 
 rcCore_dir = dir_path + "/TritonSoccerAI"                                                            
 rcCore_cmd = "java"
@@ -41,12 +40,11 @@ for i in range(0, 6):
     vfirm_robot_id = str(i)
     run_cmd_term([vfirm_cmd, vfirm_port, vfirm_robot_id, vfirm_is_blue], vfirm_dir)
 
-time.sleep(5)
+    time.sleep(2)
 
-for i in range(0, 6):
     tritonBot_port_base = str(tritonBot_port_base_base + i * 100)
     tritonBot_vfirm_port = vfirm_port
-    run_cmd_term([tritonBot_cmd, tritonBot_virtual, tritonBot_port_base, tritonBot_vfirm_port], tritonBot_dir)
+    run_cmd_term([tritonBot_cmd, tritonBot_virtual, tritonBot_port_base, tritonBot_vfirm_port], tritonBot_dir)   
 
 time.sleep(5)
 run_cmd_term(rcCore_full_cmd, rcCore_dir)
