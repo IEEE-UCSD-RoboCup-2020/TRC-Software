9890a281c347c395365e7a3012a3f0c7e9a69645
zht043 <hoz043@ucsd.edu>
2021-01-29 05:21:20 -0800

    run everything in one terminal

--- TestScripts/test.py
+++ TestScripts/test.py
@@ -25,10 +25,17 @@ AI_Robots_IPaddr = "127.0.0.1"
 AI_Robots_IPportBase = str(tritonBot_port_base_base)
 AI_full_cmd = [AI_cmd, AI_tag, AI_file, AI_team, AI_prog_mode, AI_Robots_IPaddr, AI_Robots_IPportBase] 
 
-def run_cmd_term(cmd, cwd):
-    print("opening new terminal with command: " + ' '.join(map(str, cmd))  + " at " + cwd)
-    subprocess.Popen(["gnome-terminal", "--tab", "--"] + cmd, cwd=cwd)                                                   
-
+def run_cmd_term(cmd, cwd, tab = True):
+    if tab == True:
+        print("opening new terminal with command: " + ' '.join(map(str, cmd)))
+        subprocess.Popen(["gnome-terminal", "--tab", "--"] + cmd, cwd=cwd)                                                   
+    else:
+        print("running " + ' '.join(map(str, cmd))  + " as background process")
+        subprocess.Popen(cmd, cwd=cwd, stdout=subprocess.PIPE)                                                   
+                
+def run_java(cmd, cwd):
+    subprocess.Popen(cmd, cwd=cwd)
+        
 def clear_ports():
     for i in range(0, 6):
         vfirm_port = str(vfirm_port_base + i * 10)
@@ -42,19 +49,19 @@ clear_ports()
 for i in range(0, 6):
     vfirm_port = str(vfirm_port_base + i * 10)
     vfirm_robot_id = str(i)
-    run_cmd_term([vfirm_cmd, vfirm_port, vfirm_robot_id, vfirm_is_blue], vfirm_dir)
+    run_cmd_term([vfirm_cmd, vfirm_port, vfirm_robot_id, vfirm_is_blue], vfirm_dir, False)
     time.sleep(1)
 
 for i in range(0, 6):
     tritonBot_port_base = str(tritonBot_port_base_base + i * 100)
     vfirm_port = str(vfirm_port_base + i * 10)
     tritonBot_vfirm_port = vfirm_port
-    run_cmd_term([tritonBot_cmd, tritonBot_virtual, tritonBot_port_base, tritonBot_vfirm_port], tritonBot_dir)   
+    run_cmd_term([tritonBot_cmd, tritonBot_virtual, tritonBot_port_base, tritonBot_vfirm_port], tritonBot_dir, False)   
 
 time.sleep(2)
-run_cmd_term(AI_full_cmd, AI_dir)
+run_java(AI_full_cmd, AI_dir)
 
-print("Please go to the TritonSoccerAI terminal to interact with CLI")
+#print("Please go to the TritonSoccerAI terminal to interact with CLI")
 
 while (True):
     time.sleep(1)
