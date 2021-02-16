87403cb8710cbbd9843f2b3e4e495a0c3adb34d6
duy <duypham12241999@gmail.com>
2021-02-15 22:52:59 -0800

    check for errors in gamestates

--- OtherScripts/game_control.py
+++ OtherScripts/game_control.py
@@ -12,7 +12,8 @@ s0.connect((HOST_0, PORT_0))
 s1.connect((HOST_1, PORT_1))
 
 while True:
-    print("ENTER GAME STATE AND PARAMETERS\n")
+    print()
+    print("ENTER GAME STATE AND PARAMETERS")
     print("halt")
     print("stop")
     print("running")
@@ -24,6 +25,48 @@ while True:
     print(">>> ", end='')
     
     gsMsg = input() + '\n'
+
+    gsSplit = gsMsg.split()
+    if (gsSplit[0] == "halt"):
+        pass
+    elif (gsSplit[0] == "stop"):
+        pass
+    elif (gsSplit[0] == "running"):
+        pass
+    elif (gsSplit[0] == "freekick"):
+        if (len(gsSplit) != 2):
+            print("invalid arguments")
+            continue
+        if (gsSplit[1] != "blue" and gsSplit[1] != "yellow"):
+            print("invalid arguments")
+            continue
+    elif (gsSplit[0] == "kickoff"):
+        if (len(gsSplit) != 2):
+            print("invalid arguments")
+            continue
+        if (gsSplit[1] != "blue" and gsSplit[1] != "yellow"):
+            print("invalid arguments")
+            continue
+    elif (gsSplit[0] == "penalty"):
+        pass
+    elif (gsSplit[0] == "timeout"):
+        pass
+    elif (gsSplit[0] == "ballplacement"):
+        if (len(gsSplit) != 4):
+            print("invalid arguments")
+            continue
+        if (gsSplit[1] != "blue" and gsSplit[1] != "yellow"):
+            print("invalid arguments")
+            continue
+        try:
+            float(gsSplit[2])
+            float(gsSplit[3])
+        except ValueError:
+            print("invalid arguments")
+            continue
+    else:
+        print("invalid game state")
+        continue
 
     print("Sending gamestate and parameters", gsMsg)
 
