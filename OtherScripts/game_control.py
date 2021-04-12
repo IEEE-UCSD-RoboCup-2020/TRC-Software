194a7f2af1b2d57d960aa2502c2fe24e7296f2b3
duy <duypham12241999@gmail.com>
2021-04-11 23:02:44 -0700

    Have python GameController use SSL commands

--- OtherScripts/game_control.py
+++ OtherScripts/game_control.py
@@ -14,53 +14,72 @@ s1.connect((HOST_1, PORT_1))
 while True:
     print()
     print("ENTER GAME STATE AND PARAMETERS")
-    print("halt")
-    print("stop")
-    print("running")
-    print("freekick <team>")
-    print("kickoff <team>")
-    print("penalty")
-    print("timeout")
-    print("ballplacement <team> <target pos x> <target pos y>")
+    print("HALT")
+    print("STOP")
+    print("NORMAL_START")
+    print("FORCE_START")
+    print("PREPARE_KICKOFF_YELLOW")
+    print("PREPARE_KICKOFF_BLUE")
+    print("PREPARE_PENALTY_YELLOW")
+    print("PREPARE_PENALTY_BLUE")
+    print("DIRECT_FREE_YELLOW")
+    print("DIRECT_FREE_BLUE")
+    print("INDIRECT_FREE_YELLOW")
+    print("INDIRECT_FREE_BLUE")
+    print("TIMEOUT_YELLOW")
+    print("TIMEOUT_BLUE")
+    print("BALL_PLACEMENT_BLUE <target pos x> <target pos y>")
+    print("BALL_PLACEMENT_YELLOW <target pos x> <target pos y>")
     print(">>> ", end='')
     
     gsMsg = input() + '\n'
 
     gsSplit = gsMsg.split()
-    if (gsSplit[0] == "halt"):
+    if (gsSplit[0] == "HALT"):
         pass
-    elif (gsSplit[0] == "stop"):
+    elif (gsSplit[0] == "STOP"):
         pass
-    elif (gsSplit[0] == "running"):
+    elif (gsSplit[0] == "NORMAL_START"):
         pass
-    elif (gsSplit[0] == "freekick"):
-        if (len(gsSplit) != 2):
-            print("invalid arguments")
-            continue
-        if (gsSplit[1] != "blue" and gsSplit[1] != "yellow"):
-            print("invalid arguments")
-            continue
-    elif (gsSplit[0] == "kickoff"):
-        if (len(gsSplit) != 2):
-            print("invalid arguments")
-            continue
-        if (gsSplit[1] != "blue" and gsSplit[1] != "yellow"):
-            print("invalid arguments")
-            continue
-    elif (gsSplit[0] == "penalty"):
+    elif (gsSplit[0] == "FORCE_START"):
         pass
-    elif (gsSplit[0] == "timeout"):
+    elif (gsSplit[0] == "PREPARE_KICKOFF_YELLOW"):
         pass
-    elif (gsSplit[0] == "ballplacement"):
-        if (len(gsSplit) != 4):
-            print("invalid arguments")
-            continue
-        if (gsSplit[1] != "blue" and gsSplit[1] != "yellow"):
+    elif (gsSplit[0] == "PREPARE_KICKOFF_BLUE"):
+        pass
+    elif (gsSplit[0] == "PREPARE_PENALTY_YELLOW"):
+        pass
+    elif (gsSplit[0] == "PREPARE_PENALTY_BLUE"):
+        pass
+    elif (gsSplit[0] == "DIRECT_FREE_YELLOW"):
+        pass
+    elif (gsSplit[0] == "DIRECT_FREE_BLUE"):
+        pass
+    elif (gsSplit[0] == "INDIRECT_FREE_YELLOW"):
+        pass
+    elif (gsSplit[0] == "INDIRECT_FREE_BLUE"):
+        pass
+    elif (gsSplit[0] == "TIMEOUT_YELLOW"):
+        pass
+    elif (gsSplit[0] == "TIMEOUT_BLUE"):
+        pass
+    elif (gsSplit[0] == "BALL_PLACEMENT_BLUE"):
+        if (len(gsSplit) != 3):
             print("invalid arguments")
             continue
         try:
+            float(gsSplit[1])
             float(gsSplit[2])
-            float(gsSplit[3])
+        except ValueError:
+            print("invalid arguments")
+            continue
+    elif (gsSplit[0] == "BALL_PLACEMENT_YELLOW"):
+        if (len(gsSplit) != 3):
+            print("invalid arguments")
+            continue
+        try:
+            float(gsSplit[1])
+            float(gsSplit[2])
         except ValueError:
             print("invalid arguments")
             continue
